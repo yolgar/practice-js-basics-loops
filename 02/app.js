b1b589fbf592f077eb16a6932d8a3ac03fe1c627
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-05-18 20:10:51 +0200

    Finish for and while loops

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,16 @@
 const x = 5;
 let result = 0;
+
+for(let i=0; i<=5; i++) {
+        result = result + i;
+        console.log(result);
+    }
+
+    // WHILE loop
+// let i = 0;
+// console.log(i);
+// while(i<=5) {
+//     result = result + i;
+//     console.log(result);
+//     i++;
+// }
