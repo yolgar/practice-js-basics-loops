f3c04c675fba8e9e6e951a08b35557c72b9a0177
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-05-18 21:35:56 +0200

    Finish while for draw lots

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,14 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x){
+    randomNumber = Math.round(Math.random() * 10);
+    iteration++;
+
+    // console.log(randomNumber, iteration);
+    // to check if we really draw lots in a range to 10
+}
+
+console.log(iteration);
